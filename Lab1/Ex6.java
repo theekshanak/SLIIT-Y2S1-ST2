f6c97837d1f3ef686fa4b91f6c1ca2f10222c384
Theekshana Kasthuriarachchi <theekshanattk@gmail.com>
2017-02-12 10:01:31 +0530

    Create Ex6.java

---
+++ Lab1/Ex6.java
@@ -0,0 +1,12 @@
+class Ex6 {
+	public static void main (String[] argus){
+  
+		double num1= 15.6 ; double num2 =5.21 ;
+		double add =  num1+num2 ;
+		double avg = add/2;
+		System.out.println(add);
+		System.out.println(avg);
+		
+	}
+
+}
