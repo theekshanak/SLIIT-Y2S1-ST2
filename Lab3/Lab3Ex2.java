42093469d75115edc5e5382de76e7ce5dc831918
Theekshana Kasthuriarachchi <theekshanattk@gmail.com>
2017-03-25 20:29:46 +0530

    Create Lab3Ex2.java

---
+++ Lab3/Lab3Ex2.java
@@ -0,0 +1,16 @@
+class Lab3Ex2{
+
+	public static void main(String[] args){
+	
+		int age=19;
+		
+		if(age>18){
+			System.out.println("Adult");
+		}else{
+			System.out.println("Child");
+		}
+	
+		
+	}
+
+}
