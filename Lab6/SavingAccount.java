93b9d8aba13fbb4c8587ee046d45d5c8b9d25577
Theekshana Kasthuriarachchi <theekshanattk@gmail.com>
2017-03-25 20:23:16 +0530

    Create SavingAccount.java

---
+++ Lab6/SavingAccount.java
@@ -0,0 +1,11 @@
+
+class SavingAccount extends Account{
+    super()
+    float intRate;
+   void showAnualInt(double amt){
+        double op;
+        op=(super.balance/100)*3.6;
+        System.out.println(op);
+    }
+
+}
