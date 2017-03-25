89755d97813f864d68c084d25e99173264c26a2f
Theekshana Kasthuriarachchi <theekshanattk@gmail.com>
2017-03-25 11:59:31 +0530

    Create lab4.1.java

---
+++ Lab4/lab4.1.java
@@ -0,0 +1,13 @@
+public class lab4{
+	
+	public static void main(String args[]){
+		System.out.print(args[0]);
+		float a;
+		int b;
+		b=Integer.parseInt(args[2]);
+		a=Float.parseFloat(args[1]);
+		System.out.print(b+"\n");
+		System.out.print(a+"\n");
+	}
+	
+}
