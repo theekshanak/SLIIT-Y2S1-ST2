3618df1f86cbb7e2f21d1c81fe875b3c630d9538
Theekshana Kasthuriarachchi <theekshanattk@gmail.com>
2017-03-25 20:24:34 +0530

    Create Lab2.java

---
+++ Lab2.java
@@ -0,0 +1,109 @@
+/*class lab2{
+
+	public static void main(String[] args){
+		 
+		 int miles=26; 
+		 int yards=385; 
+		 double km;
+		
+		 km=(miles/1.609)+((miles*1760.0)/1.609);
+		
+		 
+		 System.out.println("Miles in KM "+km); 
+		 
+	
+	}
+
+}*/
+
+/*class lab2{
+
+		static final double KMS_PER_MILE =1.609; //Global
+		static final double YARD_PER_MILE =1760;
+
+	public static void main(String[] args){
+				
+		 int miles=26; 
+		 int yards=385; 
+		 double km;
+		
+		 km=KMS_PER_MILE*(miles+yards)/KMS_PER_MILE);
+				 
+		 System.out.println("Miles in KM "+km); 
+		 
+	
+	}
+
+}*/
+
+
+/*class lab2{
+
+	public static void main(String[] args){
+		 
+		 int i=5;
+		 double d=i; // Widening Conversion, converting int to double
+		 
+		 System.out.println(d);				 
+		
+		 double a=5.75;
+		 int b=(int)a;  //Narrowing conversion, converting double to int
+		 
+		 System.out.println(b);	
+	
+	}
+
+}*/
+
+/*class lab2{
+
+	public static void main(String[] args){
+		 
+		System.out.println("===MAX VALUES===");	 
+		 
+		System.out.println("MAX VALUE IN INT :"+Integer.MAX_VALUE);	
+		System.out.println("MAX VALUE IN FLOAT :"+Float.MAX_VALUE);	
+		System.out.println("MAX VALUE IN DOUBLE :"+Double.MAX_VALUE);	
+		System.out.println("MAX VALUE IN BYTE :"+Byte.MAX_VALUE);	
+		
+		System.out.println("===MIN VALUES===");	 
+	
+		System.out.println("MIN VALUE IN INT :"+Integer.MIN_VALUE);	
+		System.out.println("MIN VALUE IN FLOAT :"+Float.MIN_VALUE);	
+		System.out.println("MIN VALUE IN DOUBLE :"+Double.MIN_VALUE);	
+		System.out.println("MIN VALUE IN BYTE :"+Byte.MIN_VALUE);	
+	}
+
+}*/
+
+/*class lab2{
+
+	public static void main(String[] args){
+		
+		String[] names=new String [5];
+		
+		names[0]="Pubudu";
+		names[1]="Naumi";
+		names[2]="Kavisha";
+		names[3]="John";
+		names[4]="Cena";
+		
+		for(int i=0;i<5;i++){
+			System.out.println(names[i]);	 
+		}
+		
+	}
+
+}*/
+
+class lab2{
+
+	public static void main(String[] args){
+		
+		String[][] table=new String [6][6];
+		
+		table[0][]={{"DIT","STII","SPD","PS","DCCNIISEI"}};
+		
+	}
+
+}
