5c273992545f11e39747d8dda65d4f21a4a5693f
Theekshana Kasthuriarachchi <theekshanattk@gmail.com>
2017-04-23 11:32:42 +0530

    Create Prg.java

---
+++ Lab7/Prg.java
@@ -0,0 +1,24 @@
+public class Prg {
+    public static void main(String[] args) {
+//        Pet p = new Pet("Lissie", "Smith", 3);
+//        p.showDetails();
+//        Cat c = new Cat("Kyan", "Silva", 4, 4);
+//        c.showDetails();
+//        Pet p1=new Pet("Micky" , "Chathura");
+//        p1.showDetails();
+//        Cat c1 = new Cat("Lassy", "Dilan");
+//        c1.showDetails();
+        Dog d1=new Dog("Stela","Dilan",4,2);
+        d1.showDetails();
+//        Dog d2=new Dog("Muffy","Kushani");
+//        d2.showDetails();
+        Rottweiler r1=new Rottweiler("Rosi", "Chamali",2,1,12);
+        r1.showDetails();
+//        Rottweiler r2=new Rottweiler("Rosa", "Chamal");
+//        r2.showDetails();
+
+//        Pet p3 = c1;
+//        p3.showDetails();
+
+    }
+}
